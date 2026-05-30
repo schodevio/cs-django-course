55ac8cf2d6aff7fa3ae2699baa9aa71d78a1e224
Szymon Chodzidlo <schodevio@gmail.com>
2026-05-30 21:26:10 +0000

    Style basic blog views

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+posts = [
+    {
+        "author": "Szymek",
+        "title": "Blog Post 1",
+        "content": "Lorem ipsum",
+        "created_at": "August 27, 2018"
+    },
+    {
+        "author": "Jane",
+        "title": "Blog Post 2",
+        "content": "Dolor sit",
+        "created_at": "August 28, 2018"
+    }
+]
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {
+        "posts": posts
+    }
+
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    context = {
+        "title": "About"
+    }
+
+    return render(request, "blog/about.html", context)
